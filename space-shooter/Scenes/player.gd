08578fcc7d15b94d3c0ad607f811ef78d2d4b1b5
Henninglr <henning.leroux3@gmail.com>
2025-07-22 02:14:25 +1200

    added code for basic player movements

--- space-shooter/Scenes/player.gd
+++ space-shooter/Scenes/player.gd
@@ -1,12 +1,14 @@
 extends Node2D
+class_name PlayerShip;
 
+@export var speed := 1.0;
+@export var turnSpeed := 1.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position += Vector2(100,500) 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Vector2(1,0) * 50 * delta
-	get_node("PlayerImage").rotation += 0.1 * delta;
+	var direction = Input.get_vector("TurnLeft", "TurnRight", "MoveForward", "MoveBackward")
+	position += direction * speed * delta
