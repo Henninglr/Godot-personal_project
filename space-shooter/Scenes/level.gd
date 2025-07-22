682f904b48f2c20c0e7571b56e5f9a296762059f
Henninglr <henning.leroux3@gmail.com>
2025-07-22 13:29:31 +1200

    Added stars animation
    stars added to background that render at random location, sizes and speed

--- space-shooter/Scenes/level.gd
+++ space-shooter/Scenes/level.gd
@@ -4,6 +4,23 @@ extends Node2D
 var meteor_scene : PackedScene = load("res://Scenes/meteor.tscn")
 var laser_scene : PackedScene = load("res://Scenes/laser.tscn")
 
+func _ready() -> void:
+	# stars animation
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children(): 
+		#position
+		var random_x = rng.randi_range(0, size.x)
+		var random_y = rng.randi_range(0, size.y)
+		star.position = Vector2(random_x, random_y)
+		
+		#scale
+		var random_scale = rng.randf_range(.25, 1)
+		star.scale = Vector2(random_scale, random_scale)
+		
+		#animation speed
+		star.speed_scale = rng.randf_range(0.6, 1.4)
+
 func _on_meteor_timer_timeout() -> void:
 	#create instance of meteor
 	var meteor = meteor_scene.instantiate()
